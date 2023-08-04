c59bd1017fa729cd73b0785f52374b6e7527273e
ayuanlmo <ayuan@ayuanlmo.cn>
2023-08-04 21:23:21 +0800

    feat：选择文件组件

--- src/template/AppContent.tsx
+++ src/template/AppContent.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import AppFooter from "./AppFooter";
+import DropFile from "./DropFile";
 
 function AppContent():React.JSX.Element {
     return(
         <div className={'lmo-app-content'}>
+            <DropFile />
             <AppFooter />
         </div>
     );
---
+++ src/template/DropFile.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+function DropFile(): React.JSX.Element {
+    return (
+        <div className={'lmo-app-content-drop-file lmo_position_relative lmo_cursor_pointer'}>
+            <div className={'lmo_flex_box'}>
+                <div className={'lmo-app-content-drop-file-select-file-icon'}>
+                    <img src={require('../static/svg/select-file.svg').default} alt=""/>
+                </div>
+                <div className={'lmo-app-content-drop-file-select-file-info'}>
+                    <span>点击导入文件，或将文件拖到此处</span>
+                    <span>视频、图片、音频</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default DropFile;
