902b2e08453a3b2d4f6cc92612a51d0389e73350
ayuanlmo <ayuan@ayuanlmo.cn>
2023-08-04 18:33:30 +0800

    feat：修改底部信息

--- src/template/AppFooter.tsx
+++ src/template/AppFooter.tsx
@@ -4,9 +4,13 @@ import AppConfig from "../conf/AppConfig";
 function AppFooter(): React.JSX.Element {
     return (
         <div className={'lmo-app-footer'}>
-            <span className={'lmo_theme_color lmo_cursor_pointer'}>{AppConfig.appName}</span> &nbsp;
+            <a className={'lmo_color_white lmo_hover_theme_color'} href={AppConfig.openSource.github}
+               target={'_blank'}>{AppConfig.appName}</a>  &nbsp;
             <span>Design by 阿柒</span>
-            <span> Powered by Electron and <a className={'lmo_color_white lmo_hover_theme_color'} href="https://ffmpeg.org/" target={'_blank'}>ffmpeg</a> on Microsoft Windows</span>
+            <span> Powered by <a className={'lmo_color_white lmo_hover_theme_color'} href="https://www.electronjs.org/"
+                                 target={'_blank'}>Electron</a> and <a
+                className={'lmo_color_white lmo_hover_theme_color'} href="https://ffmpeg.org/"
+                target={'_blank'}>ffmpeg</a> on Microsoft Windows</span>
         </div>
     );
 }
