a5259080e72dcafda23e1fd343f6268cc177678d
ayuanlmo <ayuan@ayuanlmo.cn>
2023-08-11 16:25:44 +0800

    feat：警告、信息、错误提示框

--- ipc.ts
+++ ipc.ts
@@ -1,6 +1,24 @@
 import {app, BrowserWindow, dialog, ipcMain} from "electron";
+import {join} from "path";
 
 export const electronApi = 'electronApi';
+
+const createMessageBox = (win: BrowserWindow, opt: {
+    type: "warning" | "none" | "info" | "error" | "question",
+    icon?: string;
+    title?: string;
+    message: string;
+}) => {
+    const {type, icon = '', message, title = '提示信息'} = opt;
+    dialog.showMessageBoxSync(win, {
+        type: type,
+        title: title,
+        icon: icon,
+        defaultId: 0,
+        message: message,
+        buttons: ['确定']
+    });
+}
 
 export const initIpcMainHandles = (window: BrowserWindow): void => {
     ipcMain.on('HIDE-WINDOW', (): void => window.minimize());
@@ -12,9 +30,18 @@ export const initIpcMainHandles = (window: BrowserWindow): void => {
             window,
             {properties: ['openDirectory']}
         ).then((v: Electron.OpenDialogReturnValue) => {
-            if(!(v.canceled))
+            if (!(v.canceled))
                 event.sender.send('SELECTED-DIRECTORY', v.filePaths[0]);
         });
+    });
+    ipcMain.on('SHOW-ERROR-MESSAGE-BOX', (event: any, data: { msg: string; }): void => {
+        createMessageBox(window, {message: data.msg, type: 'error', title: '糟糕！出错啦'});
+    });
+    ipcMain.on('SHOW-INFO-MESSAGE-BOX', (event: any, data: string): void => {
+        createMessageBox(window, {message: data, type: 'info', icon: join("./public/favicon.ico")});
+    });
+    ipcMain.on('SHOW-WARNING-MESSAGE-BOX', (event: any, data: string): void => {
+        createMessageBox(window, {message: data, type: 'warning'});
     });
 };
 
