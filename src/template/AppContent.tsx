bf89d6b951daf78dc315fd19e5e4395657265e73
ayuanlmo <ayuan@ayuanlmo.cn>
2023-09-04 22:26:20 +0800

    feat：视频、音频分开展示

--- src/template/AppContent.tsx
+++ src/template/AppContent.tsx
@@ -4,14 +4,39 @@ import DropFile from "./DropFile";
 import Resource from "./Resource";
 import {useSelector} from "react-redux";
 import {RootState} from "../lib/Store";
+import {useEffect, useState} from "react";
 
 function AppContent(): React.JSX.Element {
     const selectedFiles = useSelector((state: RootState) => state.app.selectedFiles);
+    const globalType = useSelector((state: RootState) => state.app.globalType);
+    const [audioLength, setAudioLength] = useState<number>(0);
+    const [videoLength, setVideoLength] = useState<number>(0);
+
+    useEffect((): void => {
+        setAudioLength(selectedFiles.filter((i: {
+            streams: { codec_type: string }
+        }): boolean => {
+            return i.streams.codec_type === 'audio';
+        }).length);
+        setVideoLength(selectedFiles.filter((i: {
+            streams: { codec_type: string }
+        }): boolean => {
+            return i.streams.codec_type === 'video';
+        }).length);
+    }, [selectedFiles]);
+
+    const render = (): React.JSX.Element => {
+        const length: number = globalType === 'video' ? videoLength : audioLength;
+
+        if (length === 0)
+            return <DropFile/>;
+        return <Resource/>;
+    }
 
     return (
         <div className={'lmo-app-content'}>
             {
-                selectedFiles.length === 0 ? <DropFile/> : <Resource/>
+                selectedFiles.length === 0 ? <DropFile/> : render()
             }
             <AppFooter/>
         </div>
--- src/template/Resource.tsx
+++ src/template/Resource.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import {useEffect, useState} from 'react';
+import * as React from 'react';``
 import ResourceItem, {ResourceInfoTypes} from "./ResourceItem";
 import {useSelector} from "react-redux";
 import {RootState} from "../lib/Store";
@@ -8,22 +7,16 @@ import {GetFileInfoTypes} from "../bin/ff";
 function Resource(): React.JSX.Element {
     const selectedFiles: Array<GetFileInfoTypes> = useSelector((state: RootState) => state.app.selectedFiles);
     const globalType: string = useSelector((state: RootState) => state.app.globalType);
-    const [currentFiles, setCurrentFiles] = useState<Array<GetFileInfoTypes>>([]);
-
-    useEffect((): void => {
-        setCurrentFiles(selectedFiles.filter(i => {
-            console.log(i.streams)
-            return i.streams.codec_type === globalType
-        }))
-        console.log(selectedFiles)
-    }, [globalType]);
 
     return (
         <div className={'lmo-app-resource'}>
             <div className={'lmo-app-resource-content'}>
                 {
-                    currentFiles.map((item, index) => {
-                        return (<ResourceItem index={index} info={item as unknown as ResourceInfoTypes} key={index}/>)
+                    selectedFiles.map((item, index) => {
+                        if (item.streams.codec_type === globalType) {
+                            return (
+                                <ResourceItem index={index} info={item as unknown as ResourceInfoTypes} key={index}/>)
+                        }
                     })
                 }
             </div>
