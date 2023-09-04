30cabe9c5efbfcb0fbba57ffa29cc262990a3f13
ayuanlmo <ayuan@ayuanlmo.cn>
2023-09-04 15:52:54 +0800

    feat：增加帧信息

--- src/bin/ff.ts
+++ src/bin/ff.ts
@@ -1,14 +1,47 @@
-// 获取视频文件第一帧
-import AppConfig from "../conf/AppConfig";
-import Storage from "../lib/Storage";
-import {FFPLAY_BIN_PATH} from "./ffmpeg";
-import {FIRST_FRAME_ERROR, PLAYER_ERROR, TRANSFORM_ERROR} from "../const/Message";
-import {playBeep} from "../utils";
-
-const ffmpeg = window.require('fluent-ffmpeg');
-const fs = window.require('fs');
-const {ipcRenderer} = window.require('electron');
-const child_process = window.require('child_process');
+export interface FfmpegStreamsTypes {
+    avg_frame_rate: string;// 帧速率
+    bit_rate: number; // 比特率
+    bits_per_raw_sample: number; // 原始样本
+    chroma_location: 'left' | 'center' | 'topleft'; // 色度( 基于MPEG-2 规范
+    closed_captions: number; // 隐式字幕
+    codec_long_name: string; // 编解码器长名称 (例如：H264 / AVC / MPEG-4 AVC..
+    codec_name: string; // 编解码器名称 (例如：h264
+    codec_tag: string; // 编解码器标签
+    codec_tag_string: string; // 编解码器标签字符串
+    codec_type: 'video' | 'audio' | 'subtitle' | 'data'; // 编解码器类型 （视频(流)、音频(流)、字幕(流)、数据(流）
+    coded_height: number; // 编码高度
+    coded_width: number; // 编码宽度
+    color_primaries: 'bt709' | 'bt470m' | 'bt470bg' | 'smpte170m' | 'smpte240m'; // 色彩原色
+    color_range: 'tv' | 'limited' | 'full';// 色彩范围
+    color_space: 'bt709' | 'bt601' | 'srgb' | 'bt2020nc' | 'smpte170m' | 'smpte240m'; // 色彩空间
+    color_transfer: string; // 视频滤镜
+    display_aspect_ratio: string;// 显示宽高比
+    duration: number; // 持续时间
+    duration_ts: number; // 流持续时间
+    extradata_size: number; // 额外数据大小
+    field_order: string; // 字段顺序
+    film_grain: string; // 胶片颗粒
+    has_b_frames: number; // B帧
+    height: number; // 高度
+    id: string; // id
+    index: number; // 索引
+    is_avc: 'true' | 'false'; // 是AVC
+    level: number; // 登记
+    max_bit_rate: string | number; // 最大比特率
+    nal_length_size: number; // 最终长度大小
+    nb_frames: number; // 视频流中的帧数（包含 I帧 、P帧 、 B帧
+    start_pts: number; // 开始时间戳
+    start_time: number;// 开始时间
+    tags: {
+        creation_time: string; // 创建时间
+        encoder: string; // 编码器
+        handler_name: string; // 处理程序名
+        language: string; // 语言
+        vendor_id: string; // ?
+    }
+    time_base: string; // 时基
+    width: number; // 宽度
+}
 
 export interface GetFileInfoTypes {
     size: number | string;
@@ -16,6 +49,7 @@ export interface GetFileInfoTypes {
     width: number;
     height: number;
     format: Array<string>;
+    streams: FfmpegStreamsTypes;
 }
 
 export interface Codes {
@@ -27,6 +61,18 @@ export interface Codes {
     type: 'audio' | 'video';// 有损
     intraFrameOnly?: boolean;// 仅帧内
 }
+
+// 获取视频文件第一帧
+import AppConfig from "../conf/AppConfig";
+import Storage from "../lib/Storage";
+import {FFPLAY_BIN_PATH} from "./ffmpeg";
+import {FIRST_FRAME_ERROR, PLAYER_ERROR, TRANSFORM_ERROR} from "../const/Message";
+import {playBeep} from "../utils";
+
+const ffmpeg = window.require('fluent-ffmpeg');
+const fs = window.require('fs');
+const {ipcRenderer} = window.require('electron');
+const child_process = window.require('child_process');
 
 /**
  * @method getVideoFirstFrame
@@ -70,7 +116,7 @@ export const getVideoFirstFrame = (inputFilePath: string): Promise<string> => {
 /**
  * @method getFileInfo
  * @param {string} filePath - 文件路径
- * @returns {Promise<{size:number,duration:number,width:number,height:number,format:[]>}>}
+ * @returns {Promise<{size:number,duration:number,width:number,height:number,format:[],streams:<FfmpegStreamsTypes>>}>}
  * @description 获取文件信息
  * **/
 export const getFileInfo = (filePath: string): Promise<GetFileInfoTypes> => {
@@ -85,7 +131,8 @@ export const getFileInfo = (filePath: string): Promise<GetFileInfoTypes> => {
                 duration: data.format.duration,
                 width: data.streams[0].width,
                 height: data.streams[0].height,
-                format: data.format.format_name
+                format: data.format.format_name,
+                streams: data.streams[0]
             });
         });
     });
@@ -158,7 +205,7 @@ export const ffplayer = (path: string): void => {
             ipcRenderer.send('SHOW-ERROR-MESSAGE-BOX', {
                 msg: PLAYER_ERROR(path, err)
             });
-    })
+    });
 }
 
 // 获取可用的编解码器
@@ -174,8 +221,7 @@ export const getAvailableCodecs = (): Promise<Array<Codes>> => {
                 const {type} = codes[i];
 
                 if (type === 'audio' || type === 'video') {
-                    _.push({...codes[i]})
-                    console.log('音频')
+                    _.push({...codes[i]});
                 }
             });
             resolve(_);
