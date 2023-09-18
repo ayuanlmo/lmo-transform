8bed607fa983d836689b83b2e7429353de15c640
ayuanlmo <ayuan@ayuanlmo.cn>
2023-09-18 17:18:29 +0800

    feat：添加Button组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,12 +2,14 @@ import YSwitch from "../components/YSwitch/YSwitch";
 import Dialog from "../components/Dialog";
 import YUrlPromptInput from "../components/YUrlPromptInput";
 import YExtendTemplate from "../components/YExtendTemplate";
+import YButton from "../components/YButton/YButton";
 
 export {
     YSwitch,
     Dialog,
     YUrlPromptInput,
-    YExtendTemplate
+    YExtendTemplate,
+    YButton
 };
 
 export * from '../components';
