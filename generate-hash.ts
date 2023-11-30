8bc311b7e5b43f57d07039feceafbe6ae5b838c2
ayuanlmo <ayuan@ayuanlmo.cn>
2023-11-30 17:46:02 +0800

    feat：生成文件hash

--- generate-hash.ts
+++ generate-hash.ts
@@ -2,15 +2,15 @@ import {createHash, Hash} from 'crypto';
 import * as FS from 'fs';
 
 ((): void => {
-    let isExists: boolean = false;
+    try {
+        const HASH_MD5: Hash = createHash('md5'),
+            HASH_SHA256: Hash = createHash('sha256'),
+            FILE: string = FS.readdirSync('../dist').filter((i: string) => {
+                if (!i.includes('blockmap'))
+                    return i.includes('lmo-Transform Setup')
+            })[0];
 
-    const FILE_NAME: string = `lmo-Transform-${require('./package.json').version} Setup.exe`,
-        FILE_PATH: string = './dist/' + FILE_NAME,
-        HASH_MD5: Hash = createHash('md5'),
-        HASH_SHA256: Hash = createHash('sha256');
-
-    const Generate: Function = (): void => {
-        FS.createReadStream(FILE_PATH).on('data', (_DATA: BufferEncoding): void => {
+        FS.createReadStream('../dist/' + FILE).on('data', (_DATA: BufferEncoding): void => {
             HASH_MD5.update(_DATA);
             HASH_SHA256.update(_DATA);
         }).on('end', (): void => {
@@ -18,8 +18,8 @@ import * as FS from 'fs';
                 SHA: string = HASH_SHA256.digest('hex').toUpperCase();
 
             try {
-                FS.writeFileSync(`./dist/lmo-Transform-MD5.txt`, MD5, {encoding: 'utf8'});
-                FS.writeFileSync(`./dist/lmo-Transform-SHA-256.txt`, SHA, {encoding: 'utf8'});
+                FS.writeFileSync(`../dist/lmo-Transform-MD5.txt`, MD5, {encoding: 'utf8'});
+                FS.writeFileSync(`../dist/lmo-Transform-SHA-256.txt`, SHA, {encoding: 'utf8'});
                 console.log('\n Generate hash finish...');
             } catch (_E) {
                 throw _E;
@@ -27,38 +27,7 @@ import * as FS from 'fs';
 
             require('process').exit();
         });
+    } catch (e) {
+        throw e;
     }
-
-    const WaitFileExists: Function = (): Promise<boolean> => {
-        return new Promise((resolve): void => {
-            const _ = (): void => {
-                if (isExists) {
-                    resolve(isExists);
-                } else {
-                    const list: string[] = FS.readdirSync('./dist') as string[];
-                    for (let i: number = 0; i < list.length; i++) {
-                        const item: string = list[i];
-
-                        if (!item.includes('blockmap')) {
-                            if (item.includes('exe') && item.includes('lmo-Transform Setup')) {
-                                FS.renameSync('./dist/' + item, './dist/' + FILE_NAME);
-                                isExists = true;
-
-                                setTimeout((): void => {
-                                    resolve(isExists);
-                                }, 500);
-                                return;
-                            }
-                        }
-                    }
-                    setTimeout(_, 500);
-                }
-            }
-            _();
-        });
-    }
-
-    WaitFileExists().then((): void => {
-        Generate();
-    });
 })();
