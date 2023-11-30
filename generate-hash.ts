3fc132b54c96bc03d2b398bfd407a47dac57b0c1
ayuanlmo <ayuan@ayuanlmo.cn>
2023-11-30 17:58:15 +0800

    fix：文件路径错误

--- generate-hash.ts
+++ generate-hash.ts
@@ -5,12 +5,12 @@ import * as FS from 'fs';
     try {
         const HASH_MD5: Hash = createHash('md5'),
             HASH_SHA256: Hash = createHash('sha256'),
-            FILE: string = FS.readdirSync('../dist').filter((i: string) => {
+            FILE: string = FS.readdirSync('./dist').filter((i: string) => {
                 if (!i.includes('blockmap'))
                     return i.includes('lmo-Transform Setup')
             })[0];
 
-        FS.createReadStream('../dist/' + FILE).on('data', (_DATA: BufferEncoding): void => {
+        FS.createReadStream('./dist/' + FILE).on('data', (_DATA: BufferEncoding): void => {
             HASH_MD5.update(_DATA);
             HASH_SHA256.update(_DATA);
         }).on('end', (): void => {
@@ -18,8 +18,8 @@ import * as FS from 'fs';
                 SHA: string = HASH_SHA256.digest('hex').toUpperCase();
 
             try {
-                FS.writeFileSync(`../dist/lmo-Transform-MD5.txt`, MD5, {encoding: 'utf8'});
-                FS.writeFileSync(`../dist/lmo-Transform-SHA-256.txt`, SHA, {encoding: 'utf8'});
+                FS.writeFileSync(`./dist/lmo-Transform-MD5.txt`, MD5, {encoding: 'utf8'});
+                FS.writeFileSync(`./dist/lmo-Transform-SHA-256.txt`, SHA, {encoding: 'utf8'});
                 console.log('\n Generate hash finish...');
             } catch (_E) {
                 throw _E;
